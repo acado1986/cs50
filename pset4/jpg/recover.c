40f4f6089c0e41e980452250cb724d9f6542e3f4
acado1986 <bogdan.ioan.nechifor@gmail.com>
2016-12-13 18:56:20 +0000

    not passing check 50, doesn't read 1024 bytes

---
+++ pset4/jpg/recover.c
@@ -0,0 +1,96 @@
+/**
+ * recover.c
+ *
+ * Computer Science 50
+ * Problem Set 4
+ *
+ * Recovers JPEGs from a forensic image.
+ */
+ 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+// define size of a BYTE 
+typedef uint8_t BYTE;
+
+int main()
+{
+    // open memory card file for reading
+    FILE* inptr = fopen("card.raw", "r");
+    if (inptr == NULL)
+    {
+        printf("Could not open the file card.raw for read\n");
+        return 1;
+    }
+    
+    // create a buffer of 512 bytes to store a cluster block (FAT file system)
+    BYTE buff[512];
+    
+    // variables for titles of the new files, ex. "###.jpg"
+    char title[8];
+    int file_num = -1;
+    
+    // define output file pointer outside the loop to close the last file
+    FILE* outptr = NULL;
+    
+    // read until end of the file
+    while (fread(&buff, sizeof(buff), 1, inptr) == 1)
+    {
+        // flag if a new jpeg is found
+        bool newJpg = false;
+        
+        // check if a new jpg is found
+        for (int j = 0; j <= 508; j += 4)
+        {
+            // check the first 3 bytes for a pattern
+            if (buff[j] == 0xFF && buff[j+1] == 0xD8 && buff[j+2] == 0xFF)
+            {
+                // check if the fourth byte has the values between 0xe0 and 0xef
+                if (buff[j+3] >= 0xE0 && buff[j+3] <= 0xEF)
+                {
+                    // update the jpg flag and the number of file
+                    newJpg = true;
+                    file_num++;
+                    break;
+                }
+            }
+        }
+        
+        // open a new jpg for writing
+        if (newJpg == true)
+        {
+            // close previous file if exists
+            if ( outptr != NULL)
+                fclose(outptr);
+        
+            // name of new file
+            sprintf(title, "%03d.jpg", file_num); 
+            
+            // open file for append a new data
+            outptr = fopen(title, "a");
+            if (outptr == NULL)
+            {
+                printf("Could not open the file %s for write\n", title);
+                return 2;
+            }
+        }
+       
+        // check to only write on a valid jpg file
+        if (outptr != NULL)
+            // write data to file
+            fwrite(&buff, sizeof(buff), 1, outptr);
+            
+    }
+        
+    // close the memory card file
+    fclose(inptr);
+        
+    // close remaining files
+    if (outptr != NULL)
+        fclose(outptr);
+        
+    // exit succesfully
+    return 0;
+}
