124650ce7742f84e392551ad3950ad98c624b655
acado1986 <bogdan.ioan.nechifor@gmail.com>
2017-01-16 14:03:19 +0100

    load slightly modified

--- pset6/server.c
+++ pset6/server.c
@@ -617,6 +617,13 @@ bool load(FILE* file, BYTE** content, size_t* length)
     	size_t bytes = fread(buffer, 1, BYTES, file);
     	for (;bytes != EOF; bytes = fread(buffer, 1, BYTES, file))
     	{
+		// search for errors
+		if (ferror(bytes))
+		{
+			*length = 0;
+			return false;	
+		}
+
 		// realloc memory each read
 	    	// first realloc will the same as alloc
 	    	*content = realloc(*content, *length + bytes);
@@ -632,13 +639,6 @@ bool load(FILE* file, BYTE** content, size_t* length)
 		// increase length 
 		*length += bytes;
     	}	
-
-	// search for errors
-	if (ferror)
-	{
-		*length = 0;
-		return false;	
-	}
 
     return true;
 }
@@ -725,8 +725,7 @@ bool parse(const char* line, char* abs_path, char* query)
     end_string = strchr(&begin_string[1], ' ');
     
     // variable array to hold request-target
-    // length of string including the nul char
-    int len = end_string - begin_string;
+    // length of string including the nul char int len = end_string - begin_string;
     char request_target[len];
     strncpy(request_target, &begin_string[1], len);
     request_target[len - 1] = '\0';
